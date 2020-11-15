7c6ed47dc2c471c951adbf5d50e87a899be117d0
Dpopkova <daria.ol.p@gmail.com>
2020-11-15 21:17:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-require('http').Server((req, res) => {
+const fs = require('fs')
+const path = require('path');
+require('http')
+.Server((req, res) => {
     const CORS = {
         'Access-Control-Allow-Origin': '*',
         'X-Author' : 'itmo283528',
         'Content-Type': 'text/plain; charset=UTF-8',
         'Access-Control-Allow-Methods': 'GET,POST,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-    };    
+    };  
     res.writeHead(200, CORS);
     if (req.url === '/login/') {
         return res.end('itmo283528');
-      } else if(req.url === '/sample/') {
+      } 
+    else if(req.url === '/promise/') {
         return res.end((function task(x) {
-          return x * this ** 2
+          return new Promise((resolve, reject) => {
+            if (x < 18) { resolve('yes')} else { reject('no') }
+          })
         }).toString())
       } 
-    res.end('itmo283528');
+    else if (req.url === '/fetch/') {
+        const filePath = path.join(__dirname, 'index.html');
+        res.writeHead(200, {
+          ...CORS,
+          'Content-Type': 'text/html'
+        });
+        fs.readFile(filePath, (err, html) => {
+          res.write(html);
+          res.end();
+        });
+      } 
+    else { res.end('itmo283528'); }
 })
 .listen(process.env.PORT);
